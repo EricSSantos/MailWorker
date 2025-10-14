95b73924f77dbf791e05a3fdc1628fbc4a005b0d
Eric <ericsilva333@outlook.com>
2025-10-14 00:45:07 -0300

    GetEnvironmentVariable

--- Mail.Worker/Program.cs
+++ Mail.Worker/Program.cs
@@ -5,16 +5,23 @@ using Mail.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-//Inclui variáveis de ambiente (Dokploy, Docker,etc)
-builder.Configuration.AddEnvironmentVariables();
+if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+{
+    builder.Configuration.AddEnvironmentVariables();
+}
+else
+{
+    builder.Configuration
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables();
+}
 
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("Smtp"));
 builder.Services.Configure<RabbitMqSettings>(builder.Configuration.GetSection("RabbitMQ"));
 
 builder.Services.AddSingleton<IMailService, MailService>();
 builder.Services.AddSingleton<IRabbitMQService, RabbitMQService>();
-
 builder.Services.AddHostedService<Worker>();
 
-var host = builder.Build();
-host.Run();
+builder.Build().Run();
