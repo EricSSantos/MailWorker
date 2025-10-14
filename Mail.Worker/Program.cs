6ae6b10c8e4a84a287ccddcf46e1ad39f89c78f1
Eric <ericsilva333@outlook.com>
2025-10-14 00:36:26 -0300

    Inclui variáveis de ambiente

--- Mail.Worker/Program.cs
+++ Mail.Worker/Program.cs
@@ -6,7 +6,10 @@ using Mail.Worker;
 var builder = Host.CreateApplicationBuilder(args);
 
 //Inclui variáveis de ambiente (Dokploy, Docker,etc)
-builder.Configuration.AddEnvironmentVariables();
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("Smtp"));
 builder.Services.Configure<RabbitMqSettings>(builder.Configuration.GetSection("RabbitMQ"));
