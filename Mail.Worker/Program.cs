c8ff40bbd37f8ac52b0a19f567c895540e4d7af5
Eric <ericsilva333@outlook.com>
2025-10-14 00:40:49 -0300

    Revert "Inclui variáveis de ambiente"
    This reverts commit 6ae6b10c8e4a84a287ccddcf46e1ad39f89c78f1.

--- Mail.Worker/Program.cs
+++ Mail.Worker/Program.cs
@@ -6,10 +6,7 @@ using Mail.Worker;
 var builder = Host.CreateApplicationBuilder(args);
 
 //Inclui variáveis de ambiente (Dokploy, Docker,etc)
-builder.Configuration
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-    .AddEnvironmentVariables();
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("Smtp"));
 builder.Services.Configure<RabbitMqSettings>(builder.Configuration.GetSection("RabbitMQ"));
